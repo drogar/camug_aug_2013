bdb494d6985b378163190cd54b1463766fc4ad38
Brett G. Giles <brett.giles@drogar.com>
2013-08-17 10:24:19 -0600

    Pushing new changes

--- proofs/lib/adding_a_new_tasks_to_a_task_list.rb
+++ proofs/lib/adding_a_new_tasks_to_a_task_list.rb
@@ -50,6 +50,46 @@ class Person
   initializer :name, :age, :birthdate
 end
 
+class DataStore
+  def self.instance()
+    return DataStore.new
+  end
+  
+  def save(*args)
+  end
+  def get(*args)
+    return QueryObject.new(*args)
+  end
+end
+
+class QueryObject
+  def initialize(*args)
+  end
+  def all
+    return []
+  end
+end
+
+proof 'Fetch a list of people' do
+  ds = DataStore.instance()
+
+  p1 = Person.new("Me", 25, "1999-01-01")
+  p2 = Person.new("Other", 99, "1995-01-01")
+
+  ds.save(p1,p2)
+
+  people = ds.get(:person).all()
+
+  desc "Should have two people"
+  people.prove {count == 2}
+  desc "Should have first person in list"
+  people.prove {include?(p1)}
+  desc "Should have second person in list"
+  people.prove {include?(p2)}
+end
+
+
+
 
 
 
